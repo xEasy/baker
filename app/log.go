afcfdc234a694a1b33164951d47c9360816671f1
Yijie <yijo1001@gmail.com>
2017-05-12 16:15:09 +0800

    fix mkdir logs dir

--- app/log.go
+++ app/log.go
@@ -9,7 +9,7 @@ func InitLog() {
 
 	if os.Getenv("WEB_ENV") == "production" {
 		fmt.Println("SET LOG TO FILE")
-		os.MkdirAll("logs")
+		os.MkdirAll("logs", os.FileMode(0755))
 		stdlog, err := os.OpenFile("logs/web.stdout.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 		if err != nil {
 			panic("open log stdout file fail")
